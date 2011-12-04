e9c75a4c8f7d71368c5f3e62c320bb5fd4a11606
Victor Cera <victorcera@Victors-MacBook-Air.local>
2011-12-04 02:23:24 -0500

    added requests with delayed_jobs

--- app/models/Checkcommentanswer.rb
+++ app/models/Checkcommentanswer.rb
@@ -2,6 +2,7 @@ class Checkcommentanswer < Struct.new(:ig_media_id, :media_comment_count, :photo
   
   def perform
     comments = Instagram.media_comments(ig_media_id, :access_token => access_token)
+    success = false
     if comments.count.to_i > media_comment_count.to_i + 1
       n = 0
       comments.each do |comment|
@@ -10,14 +11,18 @@ class Checkcommentanswer < Struct.new(:ig_media_id, :media_comment_count, :photo
           if comment.from.id.to_i == photo_user_id.to_i
             Photo.first(conditions: {ig_media_id: ig_media_id}).requests.first.update_attributes(:response => comment.text)
             #send_email
-          else
-            raise "wrong user answering"
+            success = true
           end
         end
       end
-    else
-      raise "no new comment"
     end
+    nb_requests = Photo.first(conditions: {ig_media_id: ig_media_id}).requests.first.nb_requests
+    Photo.first(conditions: {ig_media_id: ig_media_id}).requests.first.update_attributes(:nb_requests => nb_requests + 1)
+    #if nb_requests < 60
+    unless success == true
+      Delayed::Job.enqueue(Checkcommentanswer.new(ig_media_id, media_comment_count, photo_user_id, access_token), 0, 1.minute.from_now.getutc)
+    end
+    #elsif nb_requests < 
   end
   
   
--- app/models/request.rb
+++ app/models/request.rb
@@ -4,6 +4,7 @@ class Request
   field :question
   field :media_comment_count
   field :response
+  field :nb_requests, :type => Integer, default: 0
   
   belongs_to :photo
   has_and_belongs_to_many :users
