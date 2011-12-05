94c26e9faf3157d7756745dd682aeabe4413ebf0
Victor Cera <victorcera@Victors-MacBook-Air.local>
2011-12-05 07:21:52 -0500

    m

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,17 +2,17 @@ class PhotosController < ApplicationController
   
   def index
     if params[:id].nil?
-       @photos = Photo.new.get_last_photos(nil,100)   
+       @photos = Photo.new.get_last_photos(nil,1)   
     elsif params[:id] == "food"
-      @photos = Photo.new.get_last_photos("Food",100)
+      @photos = Photo.new.get_last_photos("Food",1)
     elsif params[:id] == "nightlife"
-      @photos = Photo.new.get_last_photos("Nightlife Spot",100)
+      @photos = Photo.new.get_last_photos("Nightlife Spot",1)
     elsif params[:id] == "entertainment"
-      @photos = Photo.new.get_last_photos("Arts & Entertainment",100)       
+      @photos = Photo.new.get_last_photos("Arts & Entertainment",1)       
     elsif params[:id] == "outdoors"
-      @photos = Photo.new.get_last_photos("Great Outdoors",100)
+      @photos = Photo.new.get_last_photos("Great Outdoors",1)
     elsif params[:id] == "myfeed"
-      @photos = Photo.new.get_last_photos("myfeed",100)
+      @photos = Photo.new.get_last_photos("myfeed",1)
     end
     #@photos = Photo.where(:user_id => "1200123")
   end
