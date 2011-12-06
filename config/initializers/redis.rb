b25d96638bba8bdd2ffd1b26620931939204e51c
Victor Cera <victorcera@Victors-MacBook-Air.local>
2011-12-05 23:59:38 -0500

    m

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse('redis://redistogo:ea140da2aecd9e0c20f410b1be6bfdb1@viperfish.redistogo.com:9774/')
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
