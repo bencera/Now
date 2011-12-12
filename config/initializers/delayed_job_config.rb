46cbefa1f0a031539427854b79de20bc60ab3fe1
Victor Cera <victorcera@Victors-MacBook-Air.local>
2011-12-11 20:07:03 -0500

    test

--- config/initializers/delayed_job_config.rb
+++
@@ -1,4 +0,0 @@
-silence_warnings do
-  Delayed::Job.const_set("MAX_ATTEMPTS", 0)
-  Delayed::Job.const_set("MAX_RUN_TIME", 10.minutes)
-end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
 uri = URI.parse('redis://redistogo:ea140da2aecd9e0c20f410b1be6bfdb1@viperfish.redistogo.com:9774/')
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-
