deae97d948b6f246e4f28ddbe15558a8a1702d05
Victor Cera <victorcera@Victors-MacBook-Air.local>
2011-12-14 00:43:16 -0500

    added user follow feeds

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -14,32 +14,60 @@ class PhotosController < ApplicationController
     else
       if params[:id].nil?
         photos = $redis.zrevrangebyscore("feed:all",Time.now.to_i,24.hours.ago.to_i)
-        @photos = photos[(n-1)*20..(n*20-1)]
+        if photos[(n-1)*20..(n*20-1)].nil?
+          @photos = []
+        else
+          @photos = photos[(n-1)*20..(n*20-1)]
+        end
         #@photos = Photo.new.get_last_photos(nil,1)   
       elsif params[:id] == "food"
         photos = $redis.zrevrangebyscore("feed:Food",Time.now.to_i,24.hours.ago.to_i)
-        @photos = photos[(n-1)*20..(n*20-1)]
+        if photos[(n-1)*20..(n*20-1)].nil?
+          @photos = []
+        else
+          @photos = photos[(n-1)*20..(n*20-1)]
+        end
         #@photos = Photo.new.get_last_photos("Food",1)
       elsif params[:id] == "nightlife"
         photos = $redis.zrevrangebyscore("feed:NightlifeSpot",Time.now.to_i,24.hours.ago.to_i)
-        @photos = photos[(n-1)*20..(n*20-1)]
+        if photos[(n-1)*20..(n*20-1)].nil?
+          @photos = []
+        else
+          @photos = photos[(n-1)*20..(n*20-1)]
+        end
         #@photos = Photo.new.get_last_photos("Nightlife Spot",1)
       elsif params[:id] == "entertainment"
         photos = $redis.zrevrangebyscore("feed:Arts&Entertainment",Time.now.to_i,24.hours.ago.to_i)
-        @photos = photos[(n-1)*20..(n*20-1)]
+        if photos[(n-1)*20..(n*20-1)].nil?
+          @photos = []
+        else
+          @photos = photos[(n-1)*20..(n*20-1)]
+        end
         #@photos = Photo.new.get_last_photos("Arts & Entertainment",1)       
       elsif params[:id] == "outdoors"
         photos = $redis.zrevrangebyscore("feed:GreatOutdoors",Time.now.to_i,24.hours.ago.to_i)
-        @photos = photos[(n-1)*20..(n*20-1)]
+        if photos[(n-1)*20..(n*20-1)].nil?
+          @photos = []
+        else
+          @photos = photos[(n-1)*20..(n*20-1)]
+        end
         #@photos = Photo.new.get_last_photos("Great Outdoors",1)
       elsif params[:id] == "answered"
         photos = $redis.zrevrangebyscore("feed:answered",Time.now.to_i,1000.hours.ago.to_i)
-        @photos = photos[(n-1)*20..(n*20-1)]
+        if photos[(n-1)*20..(n*20-1)].nil?
+          @photos = []
+        else
+          @photos = photos[(n-1)*20..(n*20-1)]
+        end
         #@photos = Photo.new.get_last_photos("Great Outdoors",1)
       elsif params[:id] == "myfeed"
         if ig_logged_in
           photos = $redis.zrevrangebyscore("userfeed:#{current_user.id}",Time.now.to_i, 24.hours.ago.to_i)
+        if photos[(n-1)*20..(n*20-1)].nil?
+          @photos = []
+        else
           @photos = photos[(n-1)*20..(n*20-1)]
+        end
         else
           @photos = []
           flash[:notice] = "You must be logged in to have a feed"
