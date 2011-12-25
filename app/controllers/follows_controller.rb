d43f4ed4383aed7f4d9fff56303f16ccab97e06c
Ben Cera <ben.broca@gmail.com>
2011-12-25 22:16:02 +0000

    added js for followers

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -19,6 +19,7 @@ class FollowsController < ApplicationController
     Venue.first(conditions: {_id: params[:id]}).photos.take(5).each do |photo|
       $redis.zadd("userfeed:#{current_user.id}", photo.time_taken, "#{photo.id.to_s}")
     end
+    @venue = Venue.first(conditions: {_id: params[:id]})
     #Resque.enqueue(Follow, current_user, params[:id])
     respond_to do |format|
       format.html { redirect_to :back }
@@ -32,6 +33,7 @@ class FollowsController < ApplicationController
     Venue.first(conditions: {_id: params[:id]}).photos.last_hours(24*7).each do |photo|
       $redis.zrem("userfeed:#{current_user.id}", "#{photo.id.to_s}")
     end
+    @venue = Venue.first(conditions: {_id: params[:id]})
     #Resque.enqueue(Unfollow, current_user, params[:id])
     respond_to do |format|
       format.html { redirect_to :back }
