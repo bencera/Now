c87f2c28a9799e2cbd27ad368b909d0b9fbf6f64
Ben Cera <ben.broca@gmail.com>
2012-01-01 18:46:51 -0500

    infinite scrolling

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -6,12 +6,13 @@ class VenuesController < ApplicationController
     else
       n = params[:page]
     end
-    n = n.to_i
+
+    require 'will_paginate/array'
     if Venue.exists?(conditions: { fs_venue_id: params[:id]})
       #if Venue already exists in the DB, fetch it.
       v = Venue.first(conditions: { fs_venue_id: params[:id]})
       n_photos = v.photos.count
-      if (n_photos - (n-1)*20) < 20
+      if (n_photos.to_i - (n.to_i-1)*20) < 20
         access_token = nil
         access_token = current_user.ig_accesstoken unless current_user.nil? #verifier.. comment faire si le mec est pas login..
         max_id = nil
@@ -21,12 +22,16 @@ class VenuesController < ApplicationController
           v.save_photo(media, nil, nil)
         end
       end
-      n_photos = v.photos.count
-      if (n_photos - (n-1)*20) <= 0
-        @photos = []
-      else
-        @photos = v.photos.order_by([:time_taken, :desc])[(n-1)*20..(n*20-1)]
-      end  
+      # n_photos = v.photos.count
+      # if (n_photos - (n-1)*20) <= 0
+      #   @photos = []
+      # else
+      #   @photos = v.photos.order_by([:time_taken, :desc])[(n-1)*20..(n*20-1)]
+      # end  
+      @photos = v.photos.paginate(:per_page => 20, :page => params[:page])
+      if request.xhr?
+        render :partial => 'partials/showphoto', :collection => @photos, :as => :photo
+      end
       @venue = v
     else
       #if venue doesnt exist, create a new one, fetch it's last IG photos, put them in the DB and then show this venue. 
@@ -34,7 +39,8 @@ class VenuesController < ApplicationController
       v.save
       if v.new? == false
         photos = v.photos.order_by([:time_taken, :desc])
-        @photos = photos[0..19]
+        # @photos = photos[0..19]
+        @photos = photos.paginate(:per_page => 20, :page => params[:page])
         @venue = v
       else
         redirect_to '/nophotos'
