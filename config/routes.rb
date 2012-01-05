bc6c66e6a6b6be613c12a14c9ac9bd702a34030a
Ben Cera <ben.broca@gmail.com>
2012-01-05 00:42:14 -0500

    new home page, photo page. minor bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -94,7 +94,7 @@ Ubimachine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'photos#index'
 
   # See how all your routes lay out with "rake routes"
 
