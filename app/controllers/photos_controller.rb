65af8217f8f79b7b616d6bc50f4e9d5d86cff10d
Ben Cera <ben.broca@gmail.com>
2012-01-14 22:14:52 -0500

    added infinite scrolling for iphone too

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,10 +9,13 @@ class PhotosController < ApplicationController
     if Rails.env == "development"
       @photos = Photo.all.limit(500).paginate(:per_page => 20, :page => params[:page])
       if request.xhr?
-        render :partial => 'partials/showphoto', :collection => @photos, :as => :photo
+        if is_mobile_device?
+          render :partial => 'partials/showphoto_mobile', :collection => @photos, :as => :photo
+        else
+          render :partial => 'partials/showphoto', :collection => @photos, :as => :photo
+        end
       end
     else
-      
       
       if params[:category].blank? #my feed
         
@@ -23,7 +26,11 @@ class PhotosController < ApplicationController
           # photo_ids.each do |photo_id|
           #   photos << Photo.first(conditions: {_id: photo_id})
           # end
-          @photos = photo_ids.paginate(:per_page => 20, :page => params[:page])
+          if is_mobile_device?
+            @photos = photo_ids.paginate(:per_page => 5, :page => params[:page])
+          else
+            @photos = photo_ids.paginate(:per_page => 20, :page => params[:page])
+          end
           @id = true
         else
           redirect_to "/photos?category=popular&city=#{current_city}"
@@ -32,33 +39,52 @@ class PhotosController < ApplicationController
         
       elsif params[:category] == "food"
         photos = Photo.where(city: current_city).where(category: "Food").order_by([[:time_taken, :desc]]).limit(500)
-        @photos = photos.paginate(:per_page => 20, :page => params[:page])
+        if is_mobile_device?
+          @photos = photos.paginate(:per_page => 5, :page => params[:page])
+        else
+          @photos = photos.paginate(:per_page => 20, :page => params[:page])
+        end
         
         
       elsif params[:category] == "nightlife"
         photos = Photo.where(city: current_city).where(category: "Nightlife Spot").order_by([[:time_taken, :desc]]).limit(500)
-        @photos = photos.paginate(:per_page => 20, :page => params[:page])
-        
+        if is_mobile_device?
+          @photos = photos.paginate(:per_page => 5, :page => params[:page])
+        else
+          @photos = photos.paginate(:per_page => 20, :page => params[:page])
+        end        
         
       elsif params[:category] == "entertainment"
         photos = Photo.where(city: current_city).where(category: "Arts & Entertainment").order_by([[:time_taken, :desc]]).limit(500)
-        @photos = photos.paginate(:per_page => 20, :page => params[:page])
-        
+        if is_mobile_device?
+          @photos = photos.paginate(:per_page => 5, :page => params[:page])
+        else
+          @photos = photos.paginate(:per_page => 20, :page => params[:page])
+        end        
              
       elsif params[:category] == "outdoors"
         photos = Photo.where(city: current_city).where(category: "Great Outdoors").order_by([[:time_taken, :desc]]).limit(500)
-        @photos = photos.paginate(:per_page => 20, :page => params[:page])
-        
+        if is_mobile_device?
+          @photos = photos.paginate(:per_page => 5, :page => params[:page])
+        else
+          @photos = photos.paginate(:per_page => 20, :page => params[:page])
+        end        
         
       elsif params[:category] == "shopping"
         photos = Photo.where(city: current_city).where(category: "Shop & Service").order_by([[:time_taken, :desc]]).limit(500)
-        @photos = photos.paginate(:per_page => 20, :page => params[:page])
-        
+        if is_mobile_device?
+          @photos = photos.paginate(:per_page => 5, :page => params[:page])
+        else
+          @photos = photos.paginate(:per_page => 20, :page => params[:page])
+        end        
         
       elsif params[:category] == "answers"
         photos = Photo.where(city: current_city).where(answered: true).order_by([[:time_taken, :desc]]).limit(500)
-        @photos = photos.paginate(:per_page => 20, :page => params[:page])
-        
+        if is_mobile_device?
+          @photos = photos.paginate(:per_page => 5, :page => params[:page])
+        else
+          @photos = photos.paginate(:per_page => 20, :page => params[:page])
+        end        
         
       # elsif params[:category] == "trending"
       #   photos = $redis.zrevrangebyscore("feed:all",Time.now.to_i,24.hours.ago.to_i)
@@ -71,11 +97,19 @@ class PhotosController < ApplicationController
         
       elsif params[:category] == "popular"
         photos = Photo.where(city: current_city).where(:useful_count.gt => 0).order_by([[:time_taken, :desc]]).limit(500)
-       @photos = photos.paginate(:per_page => 20, :page => params[:page])
-        
+        if is_mobile_device?
+          @photos = photos.paginate(:per_page => 5, :page => params[:page])
+        else
+          @photos = photos.paginate(:per_page => 20, :page => params[:page])
+        end        
       end
+      
       if request.xhr?
-        render :partial => 'partials/showphoto', :collection => @photos, :as => :photo
+        if is_mobile_device?
+          render :partial => 'partials/showphoto_mobile', :collection => @photos, :as => :photo
+        else
+          render :partial => 'partials/showphoto', :collection => @photos, :as => :photo
+        end
       end
       
       
