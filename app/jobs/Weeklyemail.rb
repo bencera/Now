29cefeda2cc01001ca62f665c4e056cfbc6007a7
Ben Cera <ben.broca@gmail.com>
2012-01-16 14:35:34 -0500

    added infinite scrolling for iphone too

--- app/jobs/Weeklyemail.rb
+++ app/jobs/Weeklyemail.rb
@@ -1,12 +1,39 @@
-class Sendquestion
+class Weeklyemail
   @queue = :weeklyemail_queue
   def self.perform()
     begin
+      time = Time.now.to_i
       User.excludes(:email => nil).each do |user|
-        client = Instagram.client(:access_token => accesstoken)
-        response = client.user_recent_media(options={:max_id => max_id})
+        client = Instagram.client(:access_token => user.ig_accesstoken)
+        n = 0
+        max_id = nil
+        email_photos = {}
+        while n == 0
+          response = client.user_recent_media(options={:max_id => max_id})
+          n = response.count
+          response.each do |media|
+            if media.created_time.to_i > time - 7*24*3600
+              n-=1
+              if Venue.exists?(conditions: {ig_venue_id: media.location.id.to_s })
+                venue = Venue.first(conditions: {ig_venue_id: media.location.id.to_s })
+                media_day = Venue.new.week_day(media.created_time.to_i)
+                photos = venue.photos.where(:time_taken.gt => (media.created_time.to_i - 3600*24)).where(:time_taken.lt => (media.created_time.to_i + 3600*24))
+                photos.each do |photo|
+                  if Venue.new.week_day(photo.time_taken.to_i) == media_day
+                    if email_photos[venue.name].nil?
+                      email_photos[venue.name] = [photo.id] unless photo.ig_media_id == media.id
+                    else
+                      email_photos[venue.name] << photo.id unless photo.ig_media_id == media.id
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+        UserMailer.weeklyemail(user, email_photos).deliver
       end
-      
+
       
       
       client = Instagram.client(:access_token => accesstoken)
