25fd0f6cc66be893cb0b25f86ffaadef274725f6
Ben Cera <ben.broca@gmail.com>
2012-02-11 14:59:29 -0500

    bug fix profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,16 @@ class UsersController < ApplicationController
   def show
     if params[:username]
       @user = User.first(conditions: {username: params[:username]})
+      useful_photo_ids = @user.usefuls.distinct(:photo_id)
+      photo_ids = []
+      photo_ids = photo_ids +  useful_photo_ids
+      photos = []
+      photo_ids.each do |photo_id|
+        photos << Photo.first(conditions: {_id: photo_id.to_s})
+      end
     elsif params[:ig_username]
       @user = User.first(conditions: {ig_username: params[:ig_username]})
-    end
-    useful_photo_ids = @user.usefuls.distinct(:photo_id)
-    photo_ids = []
-    photo_ids = photo_ids +  useful_photo_ids
-    photos = []
-    photo_ids.each do |photo_id|
-      photos << Photo.first(conditions: {_id: photo_id.to_s})
+      photos = @user.photos
     end
     @photos = photos.paginate(:per_page => 20, :page => params[:page])
     if request.xhr?
@@ -43,9 +44,54 @@ class UsersController < ApplicationController
     end
   end
   
+  def photos
+    if params[:username]
+      @user = User.first(conditions: {username: params[:username]})
+    else
+      @user = User.first(conditions: {ig_username: params[:ig_username]})
+    end
+    photos = @user.photos
+    @photos = photos.paginate(:per_page => 20, :page => params[:page])
+    if request.xhr?
+      render :partial => 'partials/showphoto', :collection => @photos, :as => :photo
+    end
+  end
+  
+  
+  def todo
+    if params[:username]
+      @user = User.first(conditions: {username: params[:username]})
+    else
+      @user = User.first(conditions: {ig_username: params[:ig_username]})
+    end 
+    photos = @user.usefuls.where(:done => nil).distinct(:photo_id)
+    @id = true
+    @photos = photos.paginate(:per_page => 20, :page => params[:page])
+    if request.xhr?
+      render :partial => 'partials/showphoto', :collection => @photos, :as => :photo
+    end
+  end
+  
+  def done
+    if params[:username]
+      @user = User.first(conditions: {username: params[:username]})
+    else
+      @user = User.first(conditions: {ig_username: params[:ig_username]})
+    end
+    photos = @user.usefuls.excludes(:done => nil).distinct(:photo_id)
+    @id = true
+    @photos = photos.paginate(:per_page => 20, :page => params[:page])
+    if request.xhr?
+      render :partial => 'partials/showphoto', :collection => @photos, :as => :photo
+    end
+  end
   
   def follows
-    @user = User.first(conditions: {ig_username: params[:ig_username]})
+    if params[:username]
+      @user = User.first(conditions: {username: params[:username]})
+    else
+      @user = User.first(conditions: {ig_username: params[:ig_username]})
+    end
     photos = []
     @user.venue_ids.each do |venue_id|
       photos << Venue.first(conditions: {_id: venue_id}).photos.first
