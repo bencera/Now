ce85653280f151e1c08ac3edd71a828e9539653d
Ben Cera <ben.broca@gmail.com>
2012-03-15 23:14:02 -0400

    fix photos

--- app/jobs/Fetchigphotos.rb
+++ app/jobs/Fetchigphotos.rb
@@ -3,18 +3,15 @@ class Fetchigphotos
   def self.perform(subscription)
     access_token = $redis.smembers("accesstokens")[rand($redis.smembers("accesstokens").size)]
     client = Instagram.client(:access_token => access_token)
-    n = 0
-    max_id = nil
+    min_id = nil
     response = nil
-    while n==0
-      response = client.geography_recent_media(subscription, options={:max_id => max_id})
-      unless response.blank?
-        n = response.count
-        max_id = response[n-1].id
-        response.each do |media|
-          if !(Photo.exists?(conditions: {ig_media_id: media.id}))
+    response = client.geography_recent_media(subscription, options={:count => "50", :min_id => $redis.get("#{subscription}:min_id")})
+    unless response.blank?
+      $redis.set("#{subscription}:min_id", response.first.id)
+      response.each do |media|
+        unless media.location.id.nil?
+          unless Photo.exists?(conditions: {ig_media_id: media.id})
             Photo.new.find_location_and_save(media,nil)
-            n -= 1
           end
         end
       end
