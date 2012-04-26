a03377a47aeeda7e985c6b2068157dcc5bf9d8c6
Ben Cera <ben.broca@gmail.com>
2012-04-25 21:32:17 -0400

    events in 4 cities

--- app/jobs/Trending.rb
+++ app/jobs/Trending.rb
@@ -1,7 +1,7 @@
 class Trending
   @queue = :trending_queue
 
-  def self.perform(hours, city)
+  def self.perform(hours)
     hours = hours.to_i
     stop_characters = ["-",".","~", "!", "&", ",", "(", ")", "#", "/", "@", ":", "?", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     stop_words = ["a", "b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
