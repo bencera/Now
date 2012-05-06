21933ba10381da23383aca66eabc79e702bec640
Ben Cera <ben.broca@gmail.com>
2012-05-06 17:00:11 -0400

    user location updated

--- app/jobs/Trending.rb
+++ app/jobs/Trending.rb
@@ -196,7 +196,6 @@ class Trending
         # if Venue.find(event.venue_id).photos.last_hours(2).count == 0
         #   event.update_attribute(:status, "not_trending")
         # end
-      end
     end  
     
      Event.where(:status => "trending").each do |event|
