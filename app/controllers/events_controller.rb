327595b3fe4265668e6a68e6d8ac1cb9b52fa946
Ben Cera <ben.broca@gmail.com>
2012-05-11 00:46:26 -0400

    notifications

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -43,50 +43,12 @@ class EventsController < ApplicationController
       event.update_attribute(:shortid, params[:event_id])
       event.update_attribute(:link, params[:link]) unless params[:link].nil?
     end 
-    event_type = params[:category]
-    case event_type
-    when "Concert"
-      emoji = ["E03E".to_i(16)].pack("U")
-    when "Party"
-      emoji = ["E047".to_i(16)].pack("U")
-    when "Sport"
-      emoji = ["E42A".to_i(16)].pack("U")
-    when "Art"
-      emoji = ["E502".to_i(16)].pack("U")
-    when "Outdoors"
-      emoji = ["E04A".to_i(16)].pack("U")
-    when "Exceptional"
-      emoji = ["E252".to_i(16)].pack("U")
-    when "Celebrity"
-      emoji = ["E51C".to_i(16)].pack("U")
-    when "Food"
-      emoji = ["E120".to_i(16)].pack("U")
-    when "Movie"
-      emoji = ["E324".to_i(16)].pack("U")
-    when "Conference"
-      emoji = ["E301".to_i(16)].pack("U")
-    when "Performance"
-      emoji = ["E503".to_i(16)].pack("U")
+    if params[:push] == 1
+      push = true
+    else
+      push = false
     end
-    
-    if params[:push] == "1"
-    if Time.now.to_i - event.end_time.to_i < 3600
-      alert = ""
-      alert = alert +  "#{emoji} " unless emoji.nil?
-      alert = alert + "#{event.description} @ #{event.venue.name}"
-      alert = alert + " (#{event.venue.neighborhood})" unless event.venue.neighborhood.nil?
-      APN::Device.all.each do |device|
-        unless device.subscriptions.first.nil?
-          n = APN::Notification.new
-          n.subscription = device.subscriptions.first
-          n.alert = alert
-          #n.sound = "none"
-          n.event = event.id
-          n.deliver
-        end
-      end
-    end 
-    end 
+    Resque.enqueue(Sendnotifications, event.id, push)
     redirect_to "http://checkthis.com/okzf"
   end
   
