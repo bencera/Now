002411a8d405eaede3ad456aeab09e44259ba225
Ben Cera <ben.broca@gmail.com>
2012-05-20 22:49:24 -0400

    geolocated devices

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,7 @@ class EventsController < ApplicationController
   end
   
   def index
-    events = Event.where(:city => params[:city]).where(:start_time.gt => 12.hours.ago.to_i).where(:status.in => ["trended", "trending"]).order_by([[:end_time, :desc]])
+    events = Event.where(:city => params[:city]).where(:end_time.gt => 12.hours.ago.to_i).where(:status.in => ["trended", "trending"]).order_by([[:end_time, :desc]])
     if events.count >= 10
       @events = events
     else
