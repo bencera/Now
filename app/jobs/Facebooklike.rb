dffe66e3b0c8a23acf014a72e65b77e80a67e41f
Ben Cera <ben.broca@gmail.com>
2012-06-20 18:22:51 -0400

    like dynamic fixed

--- app/jobs/Facebooklike.rb
+++ app/jobs/Facebooklike.rb
@@ -4,8 +4,6 @@ class Facebooklike
     response = HTTParty.post("https://graph.facebook.com/me/getnowapp:love?access_token=#{access_token}&experience=http://getnowapp.com/#{event_shortid}&end_time=2050-01-01")
     if response['id']
       $redis.set("facebook_love:#{user_id}:#{event_shortid}", response['id'])
-    else
-      Resque.enqueue_in(5.minutes, Facebooklike, access_token, event_shortid, user_id)
     end
   end
 end
--- app/jobs/Facebookunlike.rb
+++ app/jobs/Facebookunlike.rb
@@ -4,8 +4,6 @@ class Facebookunlike
     response = HTTParty.delete("https://graph.facebook.com/#{$redis.get("facebook_love:#{user_id}:#{event_shortid}")}?access_token=#{access_token}")
     if response.parsed_response == true
       $redis.del("facebook_love:#{user_id}:#{event_shortid}")
-    else
-      Resque.enqueue_in(5.minutes, Facebookunlike, access_token, event_shortid, user_id)
     end
   end
 end
