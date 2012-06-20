3f39ad22fb6599165caacde7c9066bd89e3dad25
Ben Cera <ben.broca@gmail.com>
2012-06-20 16:00:22 -0400

    facebook like

---
+++ app/jobs/Facebookunlike.rb
@@ -0,0 +1,11 @@
+class Facebooklike
+  @queue = :facebooklike_queue
+  def self.perform(access_token, event_shortid, user_id)
+    response = HTTParty.delete("https://graph.facebook.com/#{$redis.get("facebook_love:#{user_id}:#{event_shortid}")}?access_token=#{access_token}")
+    if response.parsed_response == true
+      $redis.del("facebook_love:#{user_id}:#{event_shortid}")
+    else
+      Resque.enqueue_in(5.minutes, Facebookunlike, access_token, event_shortid, user_id)
+    end
+  end
+end
