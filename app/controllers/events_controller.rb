a704a4be56abf83b562750a1c0a2a3b9b4488606
Ben Cera <ben.broca@gmail.com>
2012-06-23 00:11:27 -0400

    added likes in event.json

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,6 +17,7 @@ class EventsController < ApplicationController
   end
   
   def index
+
     events = Event.where(:city => params[:city]).where(:end_time.gt => 12.hours.ago.to_i).where(:status.in => ["trended", "trending"]).order_by([[:end_time, :desc]])
     if events.count >= 10
       @events = events
