d6980d8851622354a34f0f5537c9b2a332c92e38
Ben Cera <ben.broca@gmail.com>
2012-07-29 13:53:38 -0400

    new client Instagram..

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -18,7 +18,7 @@ class SubscriptionsController < ApplicationController
   def create
     if Rails.env.development?
       #tunnel
-      url = "http://3t8t.localtunnel.com/callbacks"
+      url = "http://4hqp.localtunnel.com/callbacks"
     else
       # production
       url = "http://pure-sky-4808.herokuapp.com/auth/instagram/callback"
--- app/jobs/Fetchigphotos.rb
+++ app/jobs/Fetchigphotos.rb
@@ -1,11 +1,12 @@
 class Fetchigphotos
   @queue = :fetchphotos_queue
   def self.perform(subscription)
-    access_token = $redis.smembers("accesstokens")[rand($redis.smembers("accesstokens").size)]
-    client = Instagram.client(:access_token => access_token)
+    # access_token = $redis.smembers("accesstokens")[rand($redis.smembers("accesstokens").size)]
+    # client = Instagram.client(:access_token => access_token)
     min_id = nil
     response = nil
-    response = client.geography_recent_media(subscription, options={:count => "50", :min_id => $redis.get("#{subscription}:min_id")})
+    response = Instagram.geography_recent_media(subscription, options={:count => "50", :min_id => $redis.get("#{subscription}:min_id")})
+    # response = client.geography_recent_media(subscription, options={:count => "50", :min_id => $redis.get("#{subscription}:min_id")})
     unless response.blank?
       $redis.set("#{subscription}:min_id", response.first.id)
       response.each do |media|
