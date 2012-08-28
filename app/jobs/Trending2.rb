a9b8d513608fd588d14f9a16c0962191b275291d
Conall O'Callaghan <ocallaghan@gmail.com>
2012-08-28 11:26:58 -0400

    changed trending2 queue just in case

--- app/jobs/Trending2.rb
+++ app/jobs/Trending2.rb
@@ -1,5 +1,5 @@
 class Trending2
-  @queue = :trending_queue
+  @queue = :trending2_queue
 
   stop_characters = ["-",".","~", "!", "&", ",", "(", ")", "#", "/", "@", ":", "<", ">", "?", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
   stop_words = ["a", "b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
