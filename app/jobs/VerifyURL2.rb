1c9c1deb6bc1bd7ac0e074276cfe8578b0ce88bb
Conall O'Callaghan <ocallaghan@gmail.com>
2012-09-07 14:10:25 -0400

    trying a different version of verifyurl

---
+++ app/jobs/VerifyURL2.rb
@@ -0,0 +1,14 @@
+class VerifyURL
+  @queue = :verifyiURL2_queue
+  def self.perform(event_id, since_time)
+      
+      unverified = Event.find(event_id).photos.where(:time_taken.gt => since_time)
+      unverified.each do |photo|
+          response = HTTParty.get(photo.url[0])
+          if response.code == 403 && response.message == "Forbidden"
+            photo.destroy
+          end
+      end  
+  end
+  
+end
