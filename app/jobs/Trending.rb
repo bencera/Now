7952cb8f04cdcaba8bc6370ed2aff82c872c0ec8
conall <ocallaghan@gmail.com>
2012-09-07 16:13:10 -0400

    added 10 minute callback

--- app/jobs/Trending.rb
+++ app/jobs/Trending.rb
@@ -222,7 +222,9 @@ class Trending
             event_i.inc(:n_photos, 1)
           end
         end
+        #add logic to make this not run if no new photos added after we know this code works
         Resque.enqueue(VerifyURL2, event_i.id, event_i.end_time)
+        Resque.enqueue_in(10.minutes, VerifyURL2, event.id, event.end_time)
         event_i.update_attribute(:end_time, Venue.find(event[0]).photos.last_hours(2).first.time_taken)
       end
     end
@@ -273,6 +275,7 @@ class Trending
             end
           end
           Resque.enqueue(VerifyURL2, event.id, event.end_time)
+          Resque.enqueue_in(10.minutes, VerifyURL2, event.id, event.end_time)
           event.update_attribute(:end_time, event.venue.photos.last_hours(2).first.time_taken) unless event.venue.photos.last_hours(2).first.nil?
         end
       end
