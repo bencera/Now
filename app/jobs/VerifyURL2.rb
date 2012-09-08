49f2074cc786eca1b52f32cb60a2734ef26d0613
conall <ocallaghan@gmail.com>
2012-09-08 18:29:44 -0400

    fixed tabbing in verifyurl2

--- app/jobs/VerifyURL2.rb
+++ app/jobs/VerifyURL2.rb
@@ -2,14 +2,13 @@ class VerifyURL2
   @queue = :verifyiURL2_queue
   def self.perform(event_id, since_time)
       
-      unverified = Event.find(event_id).photos.where(:time_taken.gt => since_time)
-      unverified.each do |photo|
-          response = HTTParty.get(photo.url[0])
-          if response.code == 403 && response.message == "Forbidden"
-            photo.destroy
-            puts "destroyed photo #{photo.id}"
-          end
-      end  
+    unverified = Event.find(event_id).photos.where(:time_taken.gt => since_time)
+    unverified.each do |photo|
+        response = HTTParty.get(photo.url[0])
+        if response.code == 403 && response.message == "Forbidden"
+          photo.destroy
+          puts "destroyed photo #{photo.id}"
+        end
+    end  
   end
-  
 end
