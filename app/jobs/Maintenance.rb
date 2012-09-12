3985866a27ae52c515504c44a0f24f7e80bd1434
conall <ocallaghan@gmail.com>
2012-09-12 14:48:42 -0400

    fixed maintenance logs a little more

--- app/jobs/Maintenance.rb
+++ app/jobs/Maintenance.rb
@@ -28,7 +28,7 @@ class Maintenance
 
         #commented out things that will change db -- CONALL
         #####event.photos.delete_if { |photo| bad_photo_list.include? photo.id }
-        Rails.logger.info("Maintenance: removed #{bad_photo_list.count} duplicate photos from event #{event.id} -- #{bad_photo_list.join("\n")}")
+        Rails.logger.info("Maintenance: removed #{bad_photo_list.count} duplicate photos from event #{event.id} -- #{bad_photo_list.join("\t")}")
 
       end
     end
