4a46ecb905a535f0de21fe21357308e8fb78dec1
conall <ocallaghan@gmail.com>
2012-09-12 12:17:18 -0400

    maintenance job written.  not debugged yet

--- app/jobs/Maintenance.rb
+++ app/jobs/Maintenance.rb
@@ -7,7 +7,6 @@ class Maintenance
 
     Event.where(:status => "trending").each do |event|
 
-
       if venue_list.include? event.venue_id
         #remove event if it's at a venue that has another event trending
         event.update_attribute(:status => "error")
@@ -16,17 +15,18 @@ class Maintenance
         #remove dupilicate photos
         
         photo_list = []
+        bad_photo_list = []
 
-        
-
-        event.photos.each_with_index do |photo, index|
-
+        event.photos.each do |photo|
+          if photo_list.include? photo.ig_media_id
+            bad_photo_list << photo.id
+          end
         end
 
+        event.photos.delete_if { |photo| bad_photo_list.include? photo.id }
+        Rails.logger.info("removed #{bad_photo_list.count} duplicate photos from event #{event.id} -- #{bad_photo_list.join("\n")}")
+
       end
-
-
-
     end
 
   end
