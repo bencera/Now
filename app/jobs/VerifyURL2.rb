26e2fe733bb694809bc073d0a2a3253e6da9e937
conall <ocallaghan@gmail.com>
2012-09-17 18:28:59 -0400

    wouldnt allow ? in resque job variable name

--- app/jobs/VerifyURL2.rb
+++ app/jobs/VerifyURL2.rb
@@ -1,13 +1,13 @@
 class VerifyURL2
   @queue = :verifyiURL2_queue
-  def self.perform(event_id, since_time, immediate?)
+  def self.perform(event_id, since_time, immediate)
       
     unverified = Event.find(event_id).photos.where(:time_taken.gt => since_time)
     unverified.each do |photo|
       response = HTTParty.get(photo.url[0])
       if response.code == 403 && response.message == "Forbidden"
         photo.destroy
-        $redis.incr(immediate? ? "MAINT_dup_events_imm" : "MAINT_dup_events_late")
+        $redis.incr(immediate ? "MAINT_dup_events_imm" : "MAINT_dup_events_late")
         puts "destroyed photo #{photo.id}"
       end
     end  
