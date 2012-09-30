5c7f7caf2fde282cfac179badf002c9d9eb33978
conall <ocallaghan@gmail.com>
2012-09-30 14:36:30 -0400

    fixed typo

--- app/jobs/AddPeopleEvent.rb
+++ app/jobs/AddPeopleEvent.rb
@@ -39,9 +39,9 @@ class AddPeopleEvent
       event.update_attribute(:illustration, illustration) if illustration
 
       # Since these should have been checked by the model, we can assume they're safe
-      event.facebook_user = FacebookUser.find(params['facebook_user_id'])
-      event.description = params['description'])
-      event.category = params['category'])  
+      event.facebook_user = FacebookUser.find(params['facebook_user_id']
+      event.description = params['description']
+      event.category = params['category']  
       event.save  
 
 
