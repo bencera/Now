3a2dce230771fc61caff014dc748924384ddc425
conall <ocallaghan@gmail.com>
2012-10-11 13:59:44 -0400

    put rescue into fetchigphotos to cut down on needless errors in resque

--- app/jobs/Fetchigphotos.rb
+++ app/jobs/Fetchigphotos.rb
@@ -6,7 +6,11 @@ class Fetchigphotos
     #client = Instagram.client(:access_token => access_token)
     min_id = nil
     response = nil
-    response = Instagram.geography_recent_media(subscription, options={:count => "50", :min_id => $redis.get("#{subscription}:min_id")})
+    begin
+      response = Instagram.geography_recent_media(subscription, options={:count => "50", :min_id => $redis.get("#{subscription}:min_id")})
+    rescue MultiJson::DecodeError => e
+      Rails.logger.error("received bad response from instagram: #{e.message}\n #{e.backtrace.inspect}")
+    end
     #response = client.geography_recent_media(subscription, options={:count => "50", :min_id => $redis.get("#{subscription}:min_id")})
     unless response.blank?
       $redis.set("#{subscription}:min_id", response.first.id)
