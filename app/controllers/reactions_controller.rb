84ee4ab9d661c2a34f061702a21265c4eaff1795
conall <ocallaghan@gmail.com>
2012-11-01 20:20:14 -0400

    ordering reactions by time

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -2,13 +2,13 @@ class ReactionsController < ApplicationController
   def index
     if params[:event_id]
       event = Event.find(params[:event_id])
-      @reactions = event.reactions
+      @reactions = event.reactions.order_by([[:created_at, :desc]]).limit(20)
     elsif params[:facebook_id]
       facebook_user = FacebookUser.where(:facebook_id => params[:facebook_id]).first
-      @reactions = facebook_user.reactions
+      @reactions = facebook_user.reactions.order_by([[:created_at, :desc]]).limit(20)
     elsif params[:nowtoken]
       facebook_user = FacebookUser.find_by_nowtoken(params[:nowtoken])
-      @reactions = facebook_user.reactions
+      @reactions = facebook_user.reactions.order_by([[:created_at, :desc]]).limit(20)
     end
   end
 
