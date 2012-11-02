73e275a5db7f038884df03b0ea87cf6294f4cc1e
conall <ocallaghan@gmail.com>
2012-11-01 20:26:59 -0400

    now users profile takes facebook id

--- app/controllers/now_users_controller.rb
+++ app/controllers/now_users_controller.rb
@@ -1,15 +1,20 @@
 class NowUsersController < ApplicationController
   
   def show
-    profile_user = FacebookUser.where(:facebook_id => params[:facebook_id]).first
+    profile_user = FacebookUser.where(:facebook_id => params[:id]).first
     fb_user = FacebookUser.find_by_nowtoken(params[:nowtoken])
     @now_profile = OpenStruct.new(profile_user.get_now_profile(profile_user))
   end
 
   def update
     @now_user = FacebookUser.find(params[:id])
+    fb_user = FacebookUser.find_by_nowtoken(params[:nowtoken])
     
-    @now_user.update_now_attributes(params)
+    if(fb_user == @now_user)
+      @now_user.update_now_attributes(params)
+    else
+      render :text => "access denied", :status => :error
+    end
 
   end
 
