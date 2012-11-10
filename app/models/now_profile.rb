f2d2f59017583112af2abb34533790a198857d66
conall <ocallaghan@gmail.com>
2012-11-10 16:45:21 -0500

    making this actually pull up your fb details

--- app/models/now_profile.rb
+++ app/models/now_profile.rb
@@ -33,12 +33,12 @@ class NowProfile
 
   def set_from_fb_details(fb_details)
     unless fb_details.nil?
-      self.name ||= fb_details["name"]
-      self.profile_photo_url ||=  "https://graph.facebook.com/#{self.fb_details['username']}/picture" 
-      self.first_name ||= fb_details["first_name"]
-      self.last_name ||= fb_details["last_name"]
-      self.email ||= fb_details["email"]
-      self.bio ||= fb_details["bio"]
+      self.name = self.name.blank? ? fb_details["name"] : self.name
+      self.profile_photo_url = self.profile_photo_url.blank? ? "https://graph.facebook.com/#{self.fb_details['username']}/picture" : self.profile_photo_url
+      self.first_name = self.first_name.blank? ? fb_details["first_name"] : self.first_name
+      self.last_name = self.last_name.blank? ? fb_details["last_name"] : self.last_name
+      self.email = self.email.blank? ? fb_details["email"] : self.email
+      self.bio = self.bio.blank? ? fb_details["bio"] : self.bio
     end
   end
 end
