c4731bf36c96959f0f5cedabb5c6840e08f2c3fe
conall <ocallaghan@gmail.com>
2012-11-10 19:51:50 -0500

    fixed after reply

--- app/models/event.rb
+++ app/models/event.rb
@@ -788,6 +788,7 @@ SCORE_HALF_LIFE       = 7.day.to_f
         description_text = first_card ? self.description : (after_reply ? "I found more photos" : "")
 
         replies << make_fake_reply(new_photo_card, description_text, timestamp)
+        after_reply = false
       else
         if checkins.count == 0
           Rails.logger.error("MAKE_REPLY_ARRAY: somehow we didn't add photos -- something messed up.")
