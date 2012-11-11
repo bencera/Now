881196319e16f5859f318a6e2f79938f44c8388b
conall <ocallaghan@gmail.com>
2012-11-11 14:46:06 -0500

    fixed bug in now profile creation

--- app/models/now_profile.rb
+++ app/models/now_profile.rb
@@ -34,7 +34,7 @@ class NowProfile
   def set_from_fb_details(fb_details)
     unless fb_details.nil?
       self.name = self.name.blank? ? fb_details["name"] : self.name
-      self.profile_photo_url = self.profile_photo_url.blank? ? "https://graph.facebook.com/#{self.fb_details['username']}/picture?type=large" : self.profile_photo_url
+      self.profile_photo_url = self.profile_photo_url.blank? ? "https://graph.facebook.com/#{fb_details['username']}/picture?type=large" : self.profile_photo_url
       self.first_name = self.first_name.blank? ? fb_details["first_name"] : self.first_name
       self.last_name = self.last_name.blank? ? fb_details["last_name"] : self.last_name
       self.email = self.email.blank? ? fb_details["email"] : self.email
