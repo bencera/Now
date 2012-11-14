9cd58bcebbc496b100ec2a6c06817349d96935fa
conall <ocallaghan@gmail.com>
2012-11-14 13:01:24 -0500

    dont users own replies in reaction list

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -11,10 +11,10 @@ class ReactionsController < ApplicationController
     elsif params[:now_id]
       facebook_user = FacebookUser.where(:now_id=> params[:now_id]).first
       @event_perspective = false
-      @reactions = facebook_user.reactions.order_by([[:created_at, :desc]]).limit(20)
+      @reactions = facebook_user.reactions.where(:reactor_id.ne => params[:now_id]).order_by([[:created_at, :desc]]).limit(20)
     elsif params[:nowtoken]
       @event_perspective = false
-      @reactions = viewer.reactions.order_by([[:created_at, :desc]]).limit(20)
+      @reactions = viewer.reactions.where(:reactor_id.ne => viewer.now_id).order_by([[:created_at, :desc]]).limit(20)
     end
   end
 
