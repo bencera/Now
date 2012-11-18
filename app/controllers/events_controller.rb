d5ff33a458f5966b1067863d72f2b4c64f59c5dc
conall <ocallaghan@gmail.com>
2012-11-18 03:48:12 -0500

    put all waiting events in insider

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -85,7 +85,8 @@ class EventsController < ApplicationController
 
   def events_trending
     if params[:city] == "world"
-      @events = Event.where(:city.in => ["newyork", "paris", "sanfrancisco", "london", "losangeles"]).where(:status => "waiting").order_by([[:n_photos, :desc]]).entries
+      #@events = Event.where(:city.in => ["newyork", "paris", "sanfrancisco", "london", "losangeles"]).where(:status => "waiting").order_by([[:n_photos, :desc]]).entries
+      @events = Event.where(:status => "waiting").order_by([[:n_photos, :desc]]).entries
     else
       @events = Event.where(:city => params[:city]).where(:status => "waiting").order_by([[:n_photos, :desc]]).entries
     end
