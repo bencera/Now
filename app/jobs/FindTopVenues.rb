e5855befb382992c2fa95a6666cda020235498d3
conall <ocallaghan@gmail.com>
2012-11-20 14:50:19 -0500

    FindTopVenues should find, populate, and create new events in whatever city its given

--- app/jobs/FindTopVenues.rb
+++ app/jobs/FindTopVenues.rb
@@ -2,7 +2,6 @@ class FindTopVenues
   @queue = :top_venues_queue
 
   require 'open-uri'
-  require 'iconv'
 
   def self.perform(in_params)
     options = in_params.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
@@ -22,7 +21,7 @@ class FindTopVenues
       near = options[:city].split(" ").join("+")
     end
 
-
+    begin_time = options[:begin_time] || 1.day.ago.to_i
 
     url = "https://api.foursquare.com/v2/venues/explore"
    
@@ -32,25 +31,97 @@ class FindTopVenues
       url = url + "?ll=#{latitude},#{longitude}"
     end
 
-    if options[:section]
-      url = url + "&section=#{options[:section]}"
-    end
-
-    url = url + "&client_id=#{Venue::FOURSQUARE_CLIENT_ID}&client_secret=#{Venue::FOURSQUARE_CLIENT_SECRET}&v=20121119"
-    #url = url + "&client_id=RFBT1TT41OW1D22SNTR21BGSWN2SEOUNELL2XKGBFLVMZ5X2&client_secret=W1FN2P3PR30DIKSWEKFEJVF51NJMZTBUY3KY3T0JNCG51QD0&v=20121119"
-
-    response = Hashie::Mash.new(JSON.parse(open(url).read))
-
-    ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+    sections = ["food", "drinks", "coffee", "shops", "arts", "outdoors", "sights", "topPicks"]
 
     venues = []
-    response.response.groups.first.items.each do |item| 
-      venue_id = ic.iconv(item.venue.id)
-      venues << (Venue.where(:_id => venue_id).first || Venue.create_venue(venue_id))
+    sections.each do |section|
+
+      url = url + "&client_id=#{Venue::FOURSQUARE_CLIENT_ID}&client_secret=#{Venue::FOURSQUARE_CLIENT_SECRET}&v=20121119"
+    #url = url + "&client_id=RFBT1TT41OW1D22SNTR21BGSWN2SEOUNELL2XKGBFLVMZ5X2&client_secret=W1FN2P3PR30DIKSWEKFEJVF51NJMZTBUY3KY3T0JNCG51QD0&v=20121119"
+
+      response = Hashie::Mash.new(JSON.parse(open(url).read))
+
+      response.response.groups.first.items.each do |item| 
+        venue_id = item.venue.id
+        venue = (Venue.where(:_id => venue_id).first || Venue.create_venue(venue_id))
+        venues << venue unless venues.include? venue
+      end
     end
 
-    venues.each {|venue| venue.update_attributes(:city => city_name)}
+    new_events = []
 
+    venues.each do |venue| 
+      venue.update_attributes(:city => city_name)
+      
+      id = venue.ig_venue_id
+      url = "https://api.instagram.com/v1/locations/" + id + "/media/recent?client_id=6c3d78eecf06493499641eb99056d175" 
+      event_found = false
+
+      while !url.nil? && !event_found
+        url = pull_more_photos(venue, url, begin_time)
+        event_found = create_event_if_possible(venue)
+      end
+
+      new_events << event_found if event_found
+    end
+
+    Rails.logger.info("created #{new_events.count} new events")
   end
 
+  def self.pull_more_photos(venue, next_url, begin_time)
+
+    url = next_url
+
+    Rails.logger.info("PopulateVenues: getting #{url}")
+   
+    begin
+      response = Hashie::Mash.new(JSON.parse(open(url).read))
+    rescue Exception => e
+      Rails.logger.error("FindTopVenues pull from instagram failed: #{e.message}")
+      return nil
+    end
+
+    data = response.data
+
+    #this way we can stop once we reach the last photo we already knew about
+    
+    data.each do |media|
+#          Photo.create_general_photo(photo_source, photo_id, photo_ts, params[:venue_id], fb_user)
+      photo = Photo.where(:ig_media_id => media.id).first || Photo.create_photo("ig", media, venue.id) 
+    end
+
+    return response.pagination.next_url
+  end
+
+  def self.create_event_if_possible(venue)
+    event_span = 3.hours.to_i
+
+    photos = venue.photos
+
+    event_photos = []
+
+    created_event = nil
+
+    event_photos << photos.pop
+
+    while photos.any? && !created_event
+
+      event_begin = event_photos.first.time_taken
+      
+      if photos.last.time_taken < event_begin + event_span
+        event_photos << photos.pop 
+      elsif event_photos.count > min_event
+        #create a new event
+        created_event = venue.create_new_event("waiting", event_photos)
+        created_event.shortid = Event.get_new_shortid
+        created_event.description = ""
+        created_event.category = "Misc"
+        created_event.keywords = []
+        created_event.save
+      else
+        event_photos.unshift
+      end
+    end
+    return created_event
+  end
 end
