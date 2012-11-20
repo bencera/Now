182da74d16ac9f7905f0b0153222dcf776fd3ca0
conall <ocallaghan@gmail.com>
2012-11-20 17:12:25 -0500

    find top venues

--- app/jobs/FindTopVenues.rb
+++ app/jobs/FindTopVenues.rb
@@ -21,26 +21,32 @@ class FindTopVenues
       near = options[:city].split(" ").join("+")
     end
 
-    begin_time = options[:begin_time] || 1.day.ago
-    begin_time = begin_time.to_i
+    min_event_photos = options[:min_event_photos] || 5
+    min_event_photos = min_event_photos.to_i
 
-    max_events = optons[:max_events] || 20
+    max_events = options[:max_events] || 40
     max_events = max_events.to_i
 
-    url = "https://api.foursquare.com/v2/venues/explore"
-   
-    if near
-      url = url + "?near=#{near}"
+    if options[:sections]
+      sections = options[:sections].split(",")
     else
-      url = url + "?ll=#{latitude},#{longitude}"
+      sections = ["food", "drinks", "coffee", "shops", "arts", "outdoors", "sights", "topPicks"]
     end
-
-    sections = ["food", "drinks", "coffee", "shops", "arts", "outdoors", "sights", "topPicks"]
-
+    
     venues = []
     sections.each do |section|
 
       Rails.logger.info("Starting section: #{section}")
+      
+      url = "https://api.foursquare.com/v2/venues/explore"
+     
+      if near
+        url = url + "?near=#{near}"
+      else
+        url = url + "?ll=#{latitude},#{longitude}"
+      end
+
+      url = url + "&section=#{section}"
 
       url = url + "&client_id=#{Venue::FOURSQUARE_CLIENT_ID}&client_secret=#{Venue::FOURSQUARE_CLIENT_SECRET}&v=20121119"
     #url = url + "&client_id=RFBT1TT41OW1D22SNTR21BGSWN2SEOUNELL2XKGBFLVMZ5X2&client_secret=W1FN2P3PR30DIKSWEKFEJVF51NJMZTBUY3KY3T0JNCG51QD0&v=20121119"
@@ -54,6 +60,22 @@ class FindTopVenues
       end
     end
 
+    return if venues.empty?
+
+    now_city = venues.first.now_city
+
+    current_time = Time.now
+
+    #set event window to noon to 4am yesterday local time
+
+    event_window_begin = now_city.new_local_time(current_time.year, current_time.month, current_time.day, 12, 0, 0).to_i
+    event_window_end = now_city.new_local_time(current_time.year, current_time.month, current_time.day + 1, 4, 0, 0).to_i
+
+    if event_window_end > current_time.to_i
+      event_window_begin -= 1.day.to_i
+      event_window_end -= 1.day.to_i
+    end
+
     new_events = []
 
     venues.each do |venue| 
@@ -63,10 +85,14 @@ class FindTopVenues
       url = "https://api.instagram.com/v1/locations/" + id + "/media/recent?client_id=6c3d78eecf06493499641eb99056d175" 
       event_found = false
 
-      while !url.nil? && !event_found && !venue.cannot_trend
-        url = pull_more_photos(venue, url, begin_time)
-        event_found = create_event_if_possible(venue)
+      photos = []
+
+      while !url.nil? 
+        url = pull_more_photos(venue, url, event_window_begin)
+        Rails.logger.info("Venue #{venue.id} now has #{venue.photos.count} photos")
       end
+
+      event_found = create_event_if_possible(venue, min_event_photos, event_window_begin, event_window_end)
 
       new_events << event_found if event_found
       break if new_events.count >= max_events
@@ -75,7 +101,14 @@ class FindTopVenues
     Rails.logger.info("created #{new_events.count} new events")
   end
 
-  def self.pull_more_photos(venue, next_url, begin_time)
+
+
+
+
+
+
+
+  def self.pull_more_photos(venue, next_url, min_time)
 
     url = next_url
 
@@ -91,44 +124,43 @@ class FindTopVenues
     data = response.data
 
     #this way we can stop once we reach the last photo we already knew about
-    
+   
+    photos = []
+
     data.each do |media|
 #          Photo.create_general_photo(photo_source, photo_id, photo_ts, params[:venue_id], fb_user)
-      photo = Photo.where(:ig_media_id => media.id).first || Photo.create_photo("ig", media, venue.id) 
+      photo = (Photo.where(:ig_media_id => media.id).first || Photo.create_photo("ig", media, venue.id))
+      photos << photo
     end
 
+    if photos.last.time_taken < min_time
+      Rails.logger.info("Photo was taken at time #{Time.at(photos.last.time_taken)} < #{Time.at(min_time)}")
+      return nil
+    end
     return response.pagination.next_url
   end
 
-  def self.create_event_if_possible(venue)
-    event_span = 3.hours.to_i
 
-    photos = venue.photos
 
-    event_photos = []
 
+
+
+
+
+  def self.create_event_if_possible(venue, min_event, begin_time, end_time)
     created_event = nil
 
-    event_photos << photos.pop
+    photos = venue.photos.where(:time_taken.gt => begin_time, :time_taken.lt => end_time).entries
 
-    while photos.any? && !created_event
-
-      event_begin = event_photos.first.time_taken
-      
-      if photos.last.time_taken < event_begin + event_span
-        event_photos << photos.pop 
-      elsif event_photos.count > min_event
-        #create a new event
-        created_event = venue.create_new_event("waiting", event_photos)
-        created_event.shortid = Event.get_new_shortid
-        created_event.description = ""
-        created_event.category = "Misc"
-        created_event.keywords = []
-        created_event.save
-      else
-        event_photos.unshift
-      end
+    if photos.count > min_event
+      created_event = venue.create_new_event("waiting", photos)
+      created_event.shortid = Event.get_new_shortid
+      created_event.description = ""
+      created_event.category = "Misc"
+      created_event.keywords = []
+      created_event.save
     end
+
     return created_event
   end
 end
