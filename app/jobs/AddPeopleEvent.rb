4a697099ea78ddaf2190bc930efdab0b11bd8210
conall <ocallaghan@gmail.com>
2012-11-29 17:58:36 -0500

    notify us when a user creates an event

--- app/jobs/AddPeopleEvent.rb
+++ app/jobs/AddPeopleEvent.rb
@@ -163,7 +163,12 @@ class AddPeopleEvent
         share_to_fs = true if params[:fs_token]
         share_to_fb = true if params[:fb_token]
 
-  
+        #### notify us if a user creates a new event
+
+        unless [BSON::ObjectId('4ffc94556ff1c9000f00000e'), BSON::ObjectId('503e79f097b4800009000003'), BSON::ObjectId('50a64cb8877a28000f000007'), BSON::ObjectId('50aa7f16212060000200000e'), nil].include? fb_user.id 
+          users_to_notify = FacebookUser.where(:now_id.in => ["1", "2"])
+          users_to_notify.each {|notify_user| notify_user.send_notification("New User event #{event.description} by user #{fb_user.now_profile.name} in city #{venue.now_city.name}, #{venue.now_city.state}, #{venue.now_city.country}", event.id) }
+        end
         
         Rails.logger.info("AddPeopleEvent created a new event #{event.id} in venue #{venue.id} -- #{venue.name} with #{photos.count} photos")
       #elsif venue.last_event.status == "trending_people"
