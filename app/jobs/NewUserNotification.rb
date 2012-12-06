688b5d9d621a1c657c55d8d3049111c9f04b0273
conall <ocallaghan@gmail.com>
2012-12-06 16:18:48 -0500

    notification is a bit cleaner

--- app/jobs/NewUserNotification.rb
+++ app/jobs/NewUserNotification.rb
@@ -7,10 +7,10 @@ class NewUserNotification
 
     device = fb_user.devices.first
 
-    if device.nil? || device.city.nil?
+    if device.nil? || (device.city.nil? && device.state.nil? && device.country.nil?)
       message = "New User #{fb_user.now_profile.name}" 
-    elsif device.city
-      message = "New User #{fb_user.now_profile.name} in #{device.city}, #{device.country}" 
+    elsif device.city || device.state || device.country
+      message = "New User #{fb_user.now_profile.name} in #{device.city}, #{device.state}, #{device.country}" 
     end
 
     FacebookUser.where(:now_id.in => ["1", "2"]).each {|user| user.send_notification(message, nil)}
