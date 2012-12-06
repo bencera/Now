010f3fb297dcbb66f750f2e2167dbce306ab31ca
conall <ocallaghan@gmail.com>
2012-12-06 18:40:45 -0500

    try to increment visits properly

--- app/helpers/now_users_helper.rb
+++ app/helpers/now_users_helper.rb
@@ -5,10 +5,7 @@ module NowUsersHelper
     
     device = APN::Device.where(:udid => params[:deviceid]).first ||  APN::Device.create!(:udid => params[:deviceid])
 
-    save_device = false
-  
     if params[:longitude] && params[:latitude]
-      device.inc(:visits, 1)
       Rails.logger.info("New visits #{device.visits}")
       device.coordinates = [params[:longitude].to_f,params[:latitude].to_f] 
       save_device = true
@@ -19,7 +16,9 @@ module NowUsersHelper
       save_device = false
     end
 
-    device.save if save_device
+    device.visits = device.visits + 1
+
+    device.save 
 
     return device
   end
