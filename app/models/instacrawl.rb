0f6fd76c2a0cde0560ce85d201fac218b557f5ce
conall <ocallaghan@gmail.com>
2013-01-08 16:45:26 -0500

    more pointed city search

--- app/models/instacrawl.rb
+++ app/models/instacrawl.rb
@@ -58,6 +58,100 @@ class Instacrawl
   end
 
 
+  #this works for the more city focused
+  def self.get_more_users_2(options={})
+
+    count = options[:count] || 50
+    token = options[:token] || "44178321.f59def8.63f2875affde4de98e043da898b6563f"
+
+    @client ||= InstagramWrapper.get_client(:access_token => token)
+
+    i = 0
+
+    user_event_list = {}
+    user_media_count = {}
+    user_event_count = {} 
+    user_name = {}
+
+    attempt = 0
+
+    begin
+
+      while i < count
+
+        i += 1
+
+        user_id = $redis.spop("SUGGESTED_CITY_USERS")
+        break if user_id.nil?
+
+        puts "examining #{user_id}"
+
+        next if $redis.sismember("ALREADY_EXAMINED_USERS", user_id)
+
+        user_info = @client.user_info(user_id)
+        
+        next if user_info.nil? || user_info.data.nil? || user_info.data.counts.nil? || user_info.data.counts.followed_by < 200
+
+        event_list = []
+        user_stats = []
+
+        find_past_potential_events(user_id, :event_list => event_list, :user_stats => user_stats)
+
+        if event_list.count >= 1
+          entry_string = "http://instagram.com/#{user_info.data.username}\t#{user_stats[0]} Photos\t#{event_list.count} Events\n#{event_list.join("\n")}"
+          puts "#{entry_string}"
+          $redis.zadd("CITY_USERS_TO_LOOK_AT", event_list.count, entry_string) 
+        end
+
+        $redis.sadd("ALREADY_EXAMINED_USERS", user_id)
+
+      end 
+    rescue Exception => e
+      if !user_id.nil? && !$redis.sismember("SUGGESTED_CITY_USERS", user_id)
+        $redis.sadd("SUGGESTED_CITY_USERS", user_id)
+      end
+      attempt += 1
+      sleep 0.5
+      puts  "#{e.message}\n#{e.backtrace.inspect}"
+      retry if attempt < 5
+    end
+  end
+
+
+
+  def self.suggest_users_from_venue(venue_id, options={})
+    venue = Venue.where(:_id => venue_id).first || Venue.create_venue(venue_id)
+
+    venue_ig_id = venue.ig_venue_id
+
+    if options[:log] 
+      Rails.logger = Logger.new(STDOUT)
+    end
+
+    users = []
+    
+    @client =  InstagramWrapper.get_client(:access_token => "44178321.f59def8.63f2875affde4de98e043da898b6563f")
+
+    end_time = options[:begin_time] || 45.weeks.ago
+    end_time = end_time.to_i
+
+    Rails.logger.info("Pulling photos from venue #{venue.name}")
+    
+    venue_media = @client.venue_media(venue_ig_id, :min_timestamp => end_time)
+    
+    begin
+      venue_media.data.each do |media|
+        users << media.user.id unless users.include?(media.user.id)
+      end
+    
+    end while venue_media.pagination && venue_media.pagination.next_url && (venue_media = @client.pull_pagination(venue_media.pagination.next_url))
+
+    users.uniq.each {|user_id| $redis.sadd("SUGGESTED_CITY_USERS", user_id)}
+  end
+
+
+
+
   def self.find_past_potential_events(user_id, options={})
     begin_time = options[:begin_time] || 1.month.ago
     begin_time = begin_time.to_i
@@ -142,6 +236,24 @@ class Instacrawl
       puts member
       puts "\n"
       $redis.zrem("USERS_TO_LOOK_AT", member)
+    end
+
+  end
+
+  def self.get_users_to_look_at_2(options={})
+    count = options[:count] || 25
+
+    total_users = $redis.zcard("CITY_USERS_TO_LOOK_AT")
+    begin_range = total_users - count
+
+    begin_range = 0 if(begin_range < 0)
+
+    members = $redis.zrange("CITY_USERS_TO_LOOK_AT", begin_range, total_users)
+
+    members.reverse.each do |member|
+      puts member
+      puts "\n"
+      $redis.zrem("CITY_USERS_TO_LOOK_AT", member)
     end
 
   end
