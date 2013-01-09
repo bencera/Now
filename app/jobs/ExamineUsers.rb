02443e8b49e5078a8dcc09424e54630357c4b670
conall <ocallaghan@gmail.com>
2013-01-09 11:53:29 -0500

    added examine_users job

---
+++ app/jobs/ExamineUsers.rb
@@ -0,0 +1,26 @@
+class ExamineUsers
+  @queue = :examine_users_queue
+
+  def self.perform(in_params)
+
+    params = in_params.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+
+    token = params[:token]
+
+    users_left = $redis.scard("SUGGESTED_CITY_USERS").to_i
+
+    return if users_left == 0
+
+    limit_remaining = $redis.get("INSTAGRAM_RATE_LIMIT_REMAINING")
+
+    if limit_remaining.to_i < 2000
+      Resque.enqueue_in(30.minutes, ExamineUsers, params)
+      return
+    end
+
+    Resque.enqueue_in(10.minutes, ExamineUsers, params)
+
+    Instacrawl.get_more_users_2(:token => token)
+
+  end
+end
