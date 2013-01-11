eae0a76675c1a045c749775d59cbb5543745b31e
conall <ocallaghan@gmail.com>
2013-01-11 16:36:14 -0500

    only notify us of trending parties, concerts, food, sports -- lower threshold

--- app/jobs/TrendingPeople.rb
+++ app/jobs/TrendingPeople.rb
@@ -29,7 +29,7 @@ class TrendingPeople
       #check event velocity
       
       if !event.reached_velocity && !event.venue.graylist
-        if event.photos.where(:time_taken.gt => 1.hour.ago.to_i).count > 5
+        if event.photos.where(:time_taken.gt => 1.hour.ago.to_i).count > 3 && ["Food", "Concert", "Party", "Sport"].include?(event.category)
           event.reached_velocity = true
           event.save!
 
