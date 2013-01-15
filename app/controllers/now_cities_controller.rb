06362ccf7894c9f49cc287c7bffc86ffe18bc732
conall <ocallaghan@gmail.com>
2013-01-15 14:40:05 -0500

    order cities

--- app/controllers/now_cities_controller.rb
+++ app/controllers/now_cities_controller.rb
@@ -3,19 +3,21 @@ class NowCitiesController < ApplicationController
     #take the redis entry, convert it from a string to a hash, then change hash keys to symbols
     city_entries = $redis.smembers("NOW_CITY_KEYS")
 
-    @cities = []
+    unordered_cities = []
 
     city_entries.each do |city_key|
 
       exp_count = $redis.get("#{city_key}_EXP")
       city_hash = $redis.hgetall("#{city_key}_VALUES")
 
-      @cities << OpenStruct.new({:name => city_hash["name"], 
+      unordered_cities << OpenStruct.new({:name => city_hash["name"], 
                                     :latitude => city_hash["latitude"].to_f,
                                     :longitude => city_hash["longitude"].to_f,
                                     :radius => city_hash["radius"].to_f,
                                     :url => city_hash["url"], :experiences => exp_count.to_i})
     end
+
+    @cities = unordered_cities.sort_by {|city| city.experiences}.reverse
 
   end
 end
