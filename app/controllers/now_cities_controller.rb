3536a0ac0a7d18d8869e4fd001794c60e31a155a
conall <ocallaghan@gmail.com>
2013-01-15 14:07:19 -0500

    fixed now cities entries

--- app/controllers/now_cities_controller.rb
+++ app/controllers/now_cities_controller.rb
@@ -1,17 +1,20 @@
 class NowCitiesController < ApplicationController
   def index
     #take the redis entry, convert it from a string to a hash, then change hash keys to symbols
-    city_entries = $redis.smembers("NOW_CITIES").map {|entry| eval(entry).inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo} }
+    city_entries = $redis.smembers("NOW_CITY_KEYS")
 
     @cities = []
 
-    city_entries.each do |entry|
-      exp_count = $redis.get(entry[:experience_key])
-      @cities << OpenStruct.new({:name => entry[:name], 
-                                    :latitude => entry[:latitude].to_f,
-                                    :longitude => entry[:longitude].to_f,
-                                    :radius => entry[:radius].to_f,
-                                    :url => entry[:url], :experiences => exp_count.to_i})
+    city_entries.each do |city_key|
+
+      exp_count = $redis.get("#{city_key}_EXP")
+      city_hash = $redis.hgetall("#{city_key}_VALUES")
+
+      @cities << OpenStruct.new({:name => city_hash["name"], 
+                                    :latitude => city_hash["latitude"].to_f,
+                                    :longitude => city_hash["longitude"].to_f,
+                                    :radius => city_hash["radius"].to_f,
+                                    :url => city_hash["url"], :experiences => exp_count.to_i})
     end
 
   end
