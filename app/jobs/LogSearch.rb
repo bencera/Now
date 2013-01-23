ea314e2c5b7dbb6f425786bd22e4c1ba9348aeee
conall <ocallaghan@gmail.com>
2013-01-23 17:12:04 -0500

    getting started with the log search job

---
+++ app/jobs/LogSearch.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+class LogSearch
+  @queue = :log_search_queue
+
+  def self.perform(in_params)
+    #params come back with string keys -- make them labels to simplify -- then make string true/false to booleans
+    params = in_params.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+
+    venue_id = params[:venue_id]
+    user_token = params[:now_token]
+    udid = params[:udid]
+
+#    if user_token
+#      fb_user = FacebookUser.where(:now_token => user_token).first
+#    elsif udid
+#      device = APN::Device.where(:udid => udid)
+#    end
+  end
+end
+
