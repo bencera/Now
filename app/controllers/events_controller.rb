c5799a6578dc18343a6a8bc927b51cf7f9f65d34
conall <ocallaghan@gmail.com>
2013-01-24 17:20:17 -0500

    whitespace

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -87,7 +87,7 @@ class EventsController < ApplicationController
 
         fake_event =  EventsHelper.get_fake_event(params[:venue_id])[:fake_event]
         
-        @events.unshift(fake_event) unless fake_event.nil?
+        @events.unshift(fake_event) unless fake_event.nil? 
       end
     elsif params[:liked_by]
       @events = EventsHelper.get_user_liked(params[:liked_by])
