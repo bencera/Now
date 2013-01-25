aba8265ad19691021410e1007c616d95fbc7415d
conall <ocallaghan@gmail.com>
2013-01-25 17:13:22 -0500

    corrected the tag for udid in event search

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -87,7 +87,7 @@ class EventsController < ApplicationController
         Resque.enqueue(LogSearch, {:search_time => Time.now.to_i, 
                                    :venue_id => params[:venue_id], 
                                    :now_token => params[:nowtoken],
-                                   :udid => params[:device_id]})
+                                   :udid => params[:deviceid]})
         if (@events.empty? || !Event::TRENDING_2_STATUSES.include?(@events.first.status))
           #create a fake event from venue activity -- and start creating it
           fake_event =  EventsHelper.get_fake_event(params[:venue_id])[:fake_event]
