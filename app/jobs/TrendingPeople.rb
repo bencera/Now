e0a335c8618fb14421220427b32fc21c7129e791
conall <ocallaghan@gmail.com>
2013-01-26 16:50:05 -0500

    fixed stupid logical bug

--- app/jobs/TrendingPeople.rb
+++ app/jobs/TrendingPeople.rb
@@ -14,7 +14,7 @@ class TrendingPeople
 
     events.each do |event|
       #if the event began today, we can keep trending it, otherwise, it's done
-      if event.began_today2?(current_time) || event.end_time > 3.hours.ago.to_i
+      if event.began_today2?(current_time) && event.end_time > 3.hours.ago.to_i
         event.fetch_and_add_photos(current_time)
       else
         event.untrend
