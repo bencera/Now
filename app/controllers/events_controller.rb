56f1a557280e7e5d5629e1c338f4729b06435ac8
conall <ocallaghan@gmail.com>
2013-01-31 10:10:12 -0500

    order events in theme by time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -73,7 +73,7 @@ class EventsController < ApplicationController
     elsif params[:theme]
       theme_id = params[:theme].to_s
       experience_ids = Theme.get_exp_list(theme_id)
-      @events = Event.find(experience_ids)
+      @events = Event.find(experience_ids).sort_by {|event| event.end_time}.reverse
     elsif params[:user_created]
       ids = [BSON::ObjectId('4ffc94556ff1c9000f00000e'), BSON::ObjectId('503e79f097b4800009000003'), BSON::ObjectId('50a64cb8877a28000f000007'), nil]
       @events =Event.where(:facebook_user_id.nin => ids, :facebook_user_id.ne => nil).order_by([[:created_at, :desc]]).limit(50).entries
