3674d385f2d4795e0d9fff5ec84fd126b593abfe
conall <ocallaghan@gmail.com>
2013-02-01 16:37:41 -0500

    wait 10 seconds before allowing a user to see no results

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -19,25 +19,24 @@ class UserSession < ActiveRecord::Base
     UserSession.where("udid = ? AND active = true", udid).each do |old_session|
       old_session.active = false
       old_session.save
-      $redis.hdel("FIRST_TOKEN_ACTION", old_session.session_token)
+      $redis.hdel("SESSION_AGE", old_session.session_token)
     end
   end
 
   def self.is_first_session_action(session_token)
-    first_action = $redis.hget("FIRST_TOKEN_ACTION", session_token).nil?
-    $redis.hset("FIRST_TOKEN_ACTION", session_token, "true") if first_action
-    return first_action
+    return $redis.hget("SESSION_AGE", session_token, "true").to_i > 10.seconds.ago
   end
 
   def self.queue_session_create(udid)
     session_token = Digest::SHA1.hexdigest([Time.now, rand].join)    
-    new_session = {:timestamp => Time.now.to_i,
+    timestamp = Time.now.to_i
+    new_session = {:timestamp => timestamp,
                    :session_token => session_token,
                    :udid => udid}
 
     $redis.sadd("NEW_SESSION_LOG", new_session)
+    $redis.hadd("SESSION_AGE", session_token, timestamp)
 
     return session_token
   end
-
 end
