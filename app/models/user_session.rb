32218e0a78e6c3feacd370f8f32ae3572347bdc2
conall <ocallaghan@gmail.com>
2013-02-03 11:47:46 -0500

    fixed bug in login created by logging session stuff

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -26,20 +26,26 @@ class UserSession < ActiveRecord::Base
   end
 
   def self.is_first_session_action(session_token)
-    return $redis.hget("SESSION_AGE", session_token).to_i > 10.seconds.ago.to_i
+    begin
+      first_action = $redis.hget("SESSION_AGE", session_token).to_i > 10.seconds.ago.to_i
+    rescue
+      return true
+    end
+    return first_action
   end
 
-  def self.queue_session_create(udid, latitude, longitude)
+  def self.queue_session_create(udid)
     session_token = Digest::SHA1.hexdigest([Time.now, rand].join)    
     timestamp = Time.now.to_i
     new_session = {:timestamp => timestamp,
                    :session_token => session_token,
-                   :udid => udid, 
-                   :latitude => latitude,
-                   :longitude => longitude}
+                   :udid => udid}
 
-    $redis.sadd("NEW_SESSION_LOG", new_session)
-    $redis.hset("SESSION_AGE", session_token, timestamp)
+    begin
+      $redis.sadd("NEW_SESSION_LOG", new_session)
+      $redis.hset("SESSION_AGE", session_token, timestamp)
+    rescue
+    end
 
     return session_token
   end
