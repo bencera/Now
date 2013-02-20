eb4e876142da273fcb4700ae4b0632a0aafd7f53
conall <ocallaghan@gmail.com>
2013-02-20 13:45:00 -0500

    fixed bug

--- app/jobs/Facebookunlike.rb
+++ app/jobs/Facebookunlike.rb
@@ -14,13 +14,14 @@ class Facebookunlike
     unless Rails.env == "development"
       response = HTTParty.delete("https://graph.facebook.com/#{$redis.get("facebook_love:#{facebook_id}:#{event_shortid}")}?access_token=#{access_token}")
       if response.parsed_response == true
-        $redis.del("facebook_love:#{user_id}:#{event_shortid}")
+        $redis.del("facebook_love:#{facebook_id}:#{event_shortid}")
       end
     end
 
     #find existing like to set as an unlike
     
-    existing_like = LikeLog.where("event_id = ? AND photo_id is NULL AND unlike = ? AND facebook_user_id = ?", event.id.to_s, false, fb_user.id.to_s).first
+    existing_like = LikeLog.where("event_id = ? AND photo_id is NULL AND unliked = ? AND facebook_user_id = ?", 
+                                  event.id.to_s, false, fb_user.id.to_s).first
 
     existing_like.unlike = true
     existing_like.save!
