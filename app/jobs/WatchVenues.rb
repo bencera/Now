49d1fae2ad57b35ebdcd54ffd006c223034c6afe
conall <ocallaghan@gmail.com>
2013-02-25 13:34:49 -0500

    another fix to venue watch

--- app/jobs/WatchVenues.rb
+++ app/jobs/WatchVenues.rb
@@ -111,7 +111,7 @@ class WatchVenue
     end
 
     FacebookUser.where(:now_id => "2").first.send_notification(
-      "WatchVenues created #{event_creation_count} new events.  Skipped #{event_skip_count}", nil) unless event_creation_count == 1
+      "WatchVenues created #{event_creation_count} new events.  Skipped #{event_skip_count}", nil) unless event_creation_count < 1
     
   end
 
