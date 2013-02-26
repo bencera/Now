b254b3249b95ee083ac126aa5bc472f9ba7ca016
conall <ocallaghan@gmail.com>
2013-02-26 17:01:39 -0500

    send a better message for pushes

--- app/models/sent_push.rb
+++ app/models/sent_push.rb
@@ -89,7 +89,7 @@ class SentPush < ActiveRecord::Base
                      )
     end
 
-    FacebookUser.where(:now_id => "2").first.send_notification("Sent Search Push to #{device_ids.count + fb_user_ids.count} users", event_id) unless (device_ids.count + fb_user_ids.count) == 0
+    FacebookUser.where(:now_id => "2").first.send_notification("Sent Push to #{device_ids.count + fb_user_ids.count} users", event_id) unless (device_ids.count + fb_user_ids.count) == 0
   end
 
   def self.batch_push(message, event_id, user_count)
