fc546e4c5b8538e87a248aa550e37a3677e827b7
conall <ocallaghan@gmail.com>
2013-04-30 11:54:40 -0400

    local_stopwords model for helping determine if a keyword is significant

---
+++ app/models/local_stopwords.rb
@@ -0,0 +1,47 @@
+class LocalStopwords
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Geocoder::Model::Mongoid
+
+  NEIGHBORHOOD_RADIUS = 1 #1km
+  CITY_RADIUS = 10
+  METROPOLITAN_RADIUS = 50
+  REGIONAL_RADIUS = 250
+  NATIONAL_RADIUS = 2000
+  CONTINENTAL_RADIUS = 10000
+
+
+  field :keyword_entries, :default => "{}"
+
+  has_many :venues
+
+  def add_keywords(venue, keyword_list)
+
+    self.venues.push(venue) unless self.venues.include?(venue)
+
+    keyword_hash = eval(self.keyword_entries)
+
+    keyword_list.each do |keyword|
+      keyword_hash[keyword_hash] ||= []
+      keyword_hash[keyword].push(venue.id) unless keyword_hash[keyword].include?(venue.id)
+    end
+
+    self.keyword_entries = keyword_list.inspect
+  end
+
+  def get_keyword_count(keyword)
+    keyword_hash = eval(self.keyword_entries)
+    if keyword_hash[keyword]
+      return keyword_hash[keyword].count
+    else
+      return 0
+    end
+  end
+
+  def get_venue_count()
+    return self.venues.count
+  end
+  
+
+  
+end
---
+++ test/unit/local_stopwords_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LocalStopwordsTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
